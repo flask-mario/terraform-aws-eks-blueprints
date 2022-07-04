e37c223cb7609835dedda633cb5c5fac817c4148
mz_flask <flask@mz.co.kr>
2022-07-04 17:31:49 +0900

    clean code

--- variables.tf
+++ variables.tf
@@ -48,7 +48,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes `<major>.<minor>` version to use for the EKS cluster (i.e.: `1.22`)"
   type        = string
-  default     = "1.22"
+  default     = "1.21"
 }
 
 #-------------------------------
